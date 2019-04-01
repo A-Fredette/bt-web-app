646bd70f2965a2d7dfe3882009610f0c27d2ce6f
A-Fredette <andrew.fredett@gmail.com>
2019-04-01 14:47:40 -0700

    began building app menu bar

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { HeaderNavigation } from '../Navigation/index'
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
 
+// TODO: Get theme application to work
+// TODO: Get the MenuIcon to work
 export default class Header extends React.Component {
-
     render() {
         return (
-            <div className='header-container'>
-                <div className='logo' />
-                <HeaderNavigation />
+            <div className='root'>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className='menuButton' color="inherit" aria-label="Menu">
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" color="inherit" className='grow'>
+                            News
+                            {/*<HeaderNavigation />*/}
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
 
     }
 };
+
