b43ee7df0473a5c58b73a14955701890362499c5
A-Fredette <andrew.fredett@gmail.com>
2019-04-01 14:51:59 -0700

    added material-UI icons library

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -6,10 +6,9 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
 
 // TODO: Get theme application to work
-// TODO: Get the MenuIcon to work
 export default class Header extends React.Component {
     render() {
         return (
