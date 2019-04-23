19242cc902fc4cc8439627a51eae8c1be1d69363
A-Fredette <andrew.fredett@gmail.com>
2019-04-22 17:38:51 -0700

    removed unecessary navbar elements

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -15,16 +15,9 @@ const AuthNav = () => (
             <Nav className="mr-auto">
                 <Link className={'nav-link'} to={ROUTES.HOME}>Home</Link>
                 <Link className={'nav-link'} to={ROUTES.LANDING}>Landing</Link>
-                <Link className={'nav-link'} to={ROUTES.ACCOUNT}>My Account</Link>
+                {/*<Link className={'nav-link'} to={ROUTES.ACCOUNT}>My Account</Link>*/}
                 <Link className={'nav-link'} to={ROUTES.WEEKLY_REVIEW}>Weekly Review</Link>
                 <SignOutButton />
-                <NavDropdown title="Dropdown Example" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
@@ -38,16 +31,9 @@ const UnauthNavBar = () => (
             <Nav className="mr-auto">
                 <Link className={'nav-link'} to={ROUTES.HOME}>Home</Link>
                 <Link className={'nav-link'} to={ROUTES.LANDING}>Landing</Link>
-                <Link className={'nav-link'} to={ROUTES.ACCOUNT}>My Account</Link>
+                {/*<Link className={'nav-link'} to={ROUTES.ACCOUNT}>My Account</Link>*/}
                 <Link className={'nav-link'} to={ROUTES.WEEKLY_REVIEW}>Weekly Review</Link>
                 <Link className={'nav-link'} to={ROUTES.SIGN_IN}>Sign In</Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
