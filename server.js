e5849b315ea8d6989a8191a495be116f93d60229
A-Fredette <andrew.fredett@gmail.com>
2019-06-10 16:01:36 -0700

    express server for heroku deployment

---
+++ server.js
@@ -0,0 +1,18 @@
+//server.js
+const express = require('express'),
+      favicon = require('express-favicon'),
+      path = require('path'),
+      port = process.env.PORT || 8080,
+      app = express();
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
