bf68d3454ff406b66c0b176b0aa9c5e555f50f77
A-Fredette <andrew.fredett@gmail.com>
2019-06-10 21:51:24 -0700

    heroku build

---
+++ build/precache-manifest.965a14a9a86c2ac9bbb2cdc585450c8d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "81ea9e469a6ac13a9f0d933c0c2307ca",
+    "url": "/static/media/furiosa.81ea9e46.jpg"
+  },
+  {
+    "revision": "11a37dc1b28921eb8895",
+    "url": "/static/css/main.26506f28.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df9665bcf57772fc9a73",
+    "url": "/static/js/2.a0f69bca.chunk.js"
+  },
+  {
+    "revision": "b8bac7b4cceefb757a28ac3f4c89e720",
+    "url": "/static/media/dawn.b8bac7b4.jpg"
+  },
+  {
+    "revision": "11a37dc1b28921eb8895",
+    "url": "/static/js/main.ed4e2ae2.chunk.js"
+  },
+  {
+    "revision": "f17573f55e7394fed6b3d489c4ef62d0",
+    "url": "/static/media/sarah.f17573f5.png"
+  },
+  {
+    "revision": "eab2f88a23022d3b17afd9236149ddca",
+    "url": "/static/media/dashboardExample.eab2f88a.png"
+  },
+  {
+    "revision": "b0828056b12809606630aaad5a9006ca",
+    "url": "/static/media/smile.b0828056.jpg"
+  },
+  {
+    "revision": "c2c62d49f2787a68a172affb5e2cbfd7",
+    "url": "/static/media/backlit.c2c62d49.jpg"
+  },
+  {
+    "revision": "1eaa33ddc05ec342d956d0eb2a920369",
+    "url": "/index.html"
+  }
+];
